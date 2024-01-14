42b2bc98ea6ae1488f0a6ddfe642b8ef44369b3d
bncandy <106873356+bncandy@users.noreply.github.com>
2024-01-14 13:39:35 +0900

    Title: 최소공배수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240114/최소공배수 구하기/find-the-least-common-multiple.c
+++ 240114/최소공배수 구하기/find-the-least-common-multiple.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
+int EUD(int a, int b) {
+	if (a == 0) return b;
+	else return EUD(b % a, a);
+}
 
-int lcd(int n, int m){
 
-    int i;
-    int result = 1;
+long long lcm(int x, int y) {
 
-    for (i = 1; i <= n; i++){
-        if( m % i == 0){
-            result = i;
-        }
-    }
+	int gcd;
+	long long result;
 
-    return n * m / result;
+	gcd = EUD(x, y);
+	result = (long long)x * y / gcd;
 
+	return result;
 }
 
 int main() {
 
     int n, m;
     scanf("%d %d", &n, &m);
-    printf("%d", lcd(n, m));
+    printf("%d", lcm(n, m));
     return 0;
 }

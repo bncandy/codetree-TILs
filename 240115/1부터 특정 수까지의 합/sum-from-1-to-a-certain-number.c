8d328df327c712eabb92825b7d14fec1a051d28f
bncandy <106873356+bncandy@users.noreply.github.com>
2024-01-15 08:40:38 +0900

    Title: 1부터 특정 수까지의 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240115/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int summary(int n){
+
+    int result = 0, i;
+
+    for (i = 1; i <= n; i++)
+        result += i;
+
+    return result;
+
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+int n;
+
+scanf("%d", &n);
+
+printf("%d", result / 10);
+    return 0;
+}

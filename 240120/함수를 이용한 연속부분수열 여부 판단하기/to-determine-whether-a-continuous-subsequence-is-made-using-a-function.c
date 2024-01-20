fa48fef07018d261f46de7dd8ddb9880e319011a
최신사탕 <106873356+bncandy@users.noreply.github.com>
2024-01-20 09:27:33 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240120/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.c
+++ 240120/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.c
@@ -1,22 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-void is_suyeol(int* a, int b, int* c, int d){
+void is_suyeol(int* a, int b, int* c, int d) {
 
     int i, j;
     int cnt = 0;
 
-    for (i = 0; i < b; i++){
-        if (a[i] == c[0]){
+    for (i = 0; i < b; i++) {
+        if (a[i] == c[0]) {
             cnt = 1;
-            for (j = 1; j <= d; j++){
-                if(c[j] == a[i + j]){
+
+            for (j = 1; j < d; j++) {
+                if (c[j] == a[i + j]) {
+
                     cnt++;
                 }
             }
         }
+        if (cnt == d) {
+            break;
+        }
     }
 
-    if(cnt != d)
+    if (cnt != d)
         printf("No");
     else printf("Yes");
 }
@@ -29,12 +35,13 @@ int main() {
 
     scanf("%d %d", &m, &n);
 
-    for(i = 0; i < m; i++)
+    for (i = 0; i < m; i++)
         scanf(" %d", &input1[i]);
-    for(i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
         scanf(" %d", &input2[i]);
-    
+
     is_suyeol(input1, m, input2, n);
+   
     // 여기에 코드를 작성해주세요.
     return 0;
 }

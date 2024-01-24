fb15e0bcb3806eeb1776426302c7e487acafca21
최신사탕 <106873356+bncandy@users.noreply.github.com>
2024-01-24 10:19:27 +0900

    Title: 두 정수에 대한 연산값, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240124/두 정수에 대한 연산값/operational-values-for-two-integers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int smallsu(int a, int b){
+
+    if (a > b)
+        return b * 2;
+    else return a * 2;
+
+}
+
+int keunsu(int a, int b){
+
+    if (a > b)
+        return a + 25;
+    else return b + 25;
+
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int a, b;
+
+    scanf("%d %d", &a, &b);
+
+    printf("%d %d", smallsu(a, b), keunsu(a, b));
+    return 0;
+}

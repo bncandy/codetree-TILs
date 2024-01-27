f3b67baae8f177f6e72bf704cbfb1c01dc97e49e
최신사탕 <106873356+bncandy@users.noreply.github.com>
2024-01-27 09:50:49 +0900

    Title: 특정 구간의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240127/특정 구간의 합/sum-of-specific-intervals.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int input[100];
+
+int main() {
+
+    int n, m;
+    int i, j;
+
+    scanf("%d %d", &n, &m);
+
+    for (i = 0; i < n; i++)
+        scanf("%d", &input[i]);
+    
+    for (i = 0; i < m; i++){
+        int a1, a2;
+        int cnt = 0;
+
+        scanf("%d %d", &a1, &a2);
+
+        for (j = a1; j <= a2; j++)
+            cnt += input[j];
+        printf("%d\n", cnt);
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}

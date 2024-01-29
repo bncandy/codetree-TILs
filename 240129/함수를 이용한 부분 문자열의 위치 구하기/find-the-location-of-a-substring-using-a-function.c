b49fffbb5983d031d96b44b40b81316b5614e12d
최신사탕 <106873356+bncandy@users.noreply.github.com>
2024-01-29 09:20:19 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240129/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char input[1001];
+char compare[1001];
+
+
+
+int main() {
+
+    char* s = NULL;
+    int i;
+
+    scanf("%s", input);
+    scanf("%s", compare);
+
+    s = strstr(input, compare);
+
+    if (!s){
+        printf("-1");
+        exit(EXIT_FAILURE);
+    }
+
+    for (i = 0; i <= strlen(input) - 1; i++){
+        if (!strcmp(s, input + i)){
+            printf("%d", i);
+            break;
+        }
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}

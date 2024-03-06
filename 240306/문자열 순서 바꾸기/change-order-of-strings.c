94f49fa3d2df6d3a901e150eb9e875323c3bbb93
최신사탕 <106873356+bncandy@users.noreply.github.com>
2024-03-06 09:37:27 +0900

    Title: 문자열 순서 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240306/문자열 순서 바꾸기/change-order-of-strings.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char buff1[101];
+    char buff2[101];
+    scanf("%s", buff1);
+    scanf("%s", buff2);
+    printf("%s\n", buff2);
+    printf("%s", buff1);
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
